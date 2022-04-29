e9b9c973b34215c97db09d483784f0e162c57d19
isaacshin92 <isaacshin92@gamil.com>
2022-04-29 18:18:43 +0900

    swea quiz solved[stack]

--- stack/stackQuiz2-2/quiz.py
+++ stack/stackQuiz2-2/quiz.py
@@ -10,33 +10,49 @@ import sys
 sys.stdin = open("input.txt")
 
 #다음 이동 경로가 미로 밖으로 나가지 않는지 체크 
-def isSefeDir(x,y):
+def isSafeDir(y,x):
     result = False 
     if x < 0 or y < 0 or x >= N or y >= N:
          result = True
     return result 
 
+def dfs(y,x):
+    global result
+    
+    if maze[y][x] == 3 or result:
+        result = 1
+        return
+    for i, j in dir:
+        dy = y + i
+        dx = x + j
+        if not isSafeDir(dy,dx):
+            
+            if maze[dy][dx] != 1: # 지나온 곳이거나, 벽이 아니면 이동 
+                maze[y][x] = 1 # 지나온 곳은 벽으로 표기 
+                dfs(dy,dx) #재귀함수 호출
 
 T = int(input())
 
 for test_case in range(1, T + 1):
-    dir = [(-1,0),(1,0),(0,-1),(0,1)]
-    x_ = 0
+    
+    dir = [(1,0),(-1,0),(0,1),(0,-1)]
+    
     y_ = 0
+    x_ = 0
+    result = 0
     
     N = int(input()) # size of maze
     maze = []
-    stack = []
     for i in range(0,N):
         maze.append(list(map(int, input())))
     
     # 시작점 구하기. 
-    for  x in range(0,N):
-        for y in range(0,N):
-            if maze[x][y] == 2:
-                x_ = x 
+    for  y in range(0,N):
+        for x in range(0,N):
+            if maze[y][x] == 2:
                 y_ = y
-    # stack.append()
-    for x, y in dir:
-        
- 
+                x_ = x 
+    dfs(y_,x_)
+    
+    print(f'#{test_case} {result}')         
+    
